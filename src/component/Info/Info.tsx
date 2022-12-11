4633b9c18f913bc9b653cb2f6fa91c9f9d1e9c17
Sourabh Jaiswal <sourabhrj31@gmail.com>
2022-12-11 17:20:16 +0800

    add: enter key instruction

--- src/component/Info/Info.tsx
+++ src/component/Info/Info.tsx
@@ -5,10 +5,18 @@ const Info = () => {
     <div className={styles.container}>
       <div className={styles.title}>Instructions</div>
       <div className={styles.body}>
-        <div>
-          <img src={'images/wasd.png'} className={styles.iconImages} />
+        <div className={styles.section}>
+          <div>
+            <img src={'images/wasd.png'} className={styles.iconImages} />
+          </div>
+          <div>Move</div>
         </div>
-        <div>To move</div>
+        <div className={styles.section}>
+          <div>
+            <img src={'images/enter_key.png'} className={styles.iconImages} />
+          </div>
+          <div>Continue</div>
+        </div>
       </div>
     </div>
   )
